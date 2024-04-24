9f740665e0b47fdee6af5b426e8f5b4f97adaeee
arya <achmadzakariya7@gmail.com>
2024-04-24 23:45:13 +0700

    p

--- app/Http/Controllers/AnalisaController.php
+++ app/Http/Controllers/AnalisaController.php
@@ -2,16 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DetailKriteria;
+use App\Models\Kriteria;
+use App\Models\Training;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class AnalisaController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('pages.analisa.index');
+        $kriteria = Kriteria::all();
+        $detailKriteria = DetailKriteria::all();
+        
+        if ($request->ajax()) {
+            $training = Training::with('penduduk')->get();
+
+            $rowData = [];
+            foreach ($training as $row) {
+                $penduduk = $row->penduduk;
+                $detailPenduduk = $penduduk->detail_penduduk;
+                $subkriteriaData = [];
+
+                foreach ($detailPenduduk as $detail) {
+                    $kriteria = Kriteria::find($detail->id_kriteria);
+                    $subkriteria = DetailKriteria::find($detail->id_subkriteria);
+
+                    if ($kriteria && $subkriteria) {
+                        $subkriteriaData[$kriteria->nama] = $subkriteria->nilai;
+                    }
+                }
+
+                $rowData[] = [
+                    'DT_RowIndex' => $row->id_training,
+                    'id_penduduk' => $penduduk->id_penduduk,
+                    'rt_rw' => $penduduk->rt_rw,
+                    'nik' => $penduduk->nik,
+                    'nama' => $penduduk->nama,
+                    'subkriteria' => $subkriteriaData,
+                    'keputusan' => $row->keputusan,
+                ];
+            }
+
+            return DataTables::of($rowData)->toJson();
+        }
+        return view('pages.analisa.index', ['kriteria' => $kriteria, 'detailKriteria' => $detailKriteria]);
     }
 
     /**
--- resources/views/pages/analisa/index.blade.php
+++ resources/views/pages/analisa/index.blade.php
@@ -26,39 +26,42 @@
                             <div class="card-header">
                                 <h3 class="card-title">Analisa Metode KNN</h3>
                             </div>
-                            <form>
+                            <form action="{{ route('training.store') }}" method="POST" onsubmit="return validateForm()">
+                                @csrf
                                 <div class="card-body">
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail1"
-                                            placeholder="Enter email">
+                                        <label for="rt">RT/RW</label>
+                                        <input type="text" class="form-control" name="rt" id="rt"
+                                            placeholder="RT/RW">
                                     </div>
                                     <div class="form-group">
-                                        <label for="exampleInputPassword1">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1"
-                                            placeholder="Password">
+                                        <label for="nik">NIK</label>
+                                        <input type="text" class="form-control" name="nik" id="nik"
+                                            placeholder="NIK">
                                     </div>
                                     <div class="form-group">
-                                        <label for="exampleInputFile">File input</label>
-                                        <div class="input-group">
-                                            <div class="custom-file">
-                                                <input type="file" class="custom-file-input" id="exampleInputFile">
-                                                <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                                            </div>
-                                            <div class="input-group-append">
-                                                <span class="input-group-text">Upload</span>
-                                            </div>
+                                        <label for="nama">Nama</label>
+                                        <input type="text" class="form-control" name="nama" id="nama"
+                                            placeholder="Nama">
+                                    </div>
+                                    @foreach ($kriteria as $kriteriaItem)
+                                        <div class="form-group">
+                                            <label for="{{ $kriteriaItem->nama }}">{{ $kriteriaItem->nama }}</label>
+                                            <select class="form-control select2" id="select2"
+                                                name="{{ str_replace(' ', '_', $kriteriaItem->nama) }}_kriteria"
+                                                style="width: 100%;" required>
+                                                <option disabled selected>Pilih
+                                                    {{ $kriteriaItem->nama }}</option>
+                                                @foreach ($detailKriteria->where('kriteria', $kriteriaItem->nama) as $detail)
+                                                    <option value="{{ $detail->id_subkriteria }}">
+                                                        {{ $detail->subkriteria }}</option>
+                                                @endforeach
+                                            </select>
                                         </div>
-                                    </div>
-                                    <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                                    </div>
+                                    @endforeach
                                 </div>
-                                <!-- /.card-body -->
-
-                                <div class="card-footer">
-                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                <div class="modal-footer justify-content-end">
+                                    <button type="submit" class="btn btn-primary">Simpan</button>
                                 </div>
                             </form>
                         </div>
@@ -78,19 +81,23 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Data Analisa</h3>
+                                <h3 class="card-title">Data Training</h3>
                             </div>
                             <div class="card-body">
-                                <table id="myTable" class="table table-bordered table-hover">
+                                <table id="myTableTraining" class="table table-bordered table-hover">
                                     <thead>
                                         <tr>
                                             <th>No</th>
-                                            <th>Nama</th>
-                                            <th>Keterangan</th>
-                                            <th>Opsi</th>
+                                            <th>RT/RW</th>
+                                            <th>NIK</th>
+                                            <th>Nama Lengkap</th>
+                                            @foreach ($kriteria as $item)
+                                                <th>{{ $item->nama }}</th>
+                                            @endforeach
+                                            <th>Keputusan</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
+                                    <tbody class="text-center">
                                     </tbody>
                                 </table>
                             </div>
@@ -105,11 +112,229 @@
             <!-- /.container-fluid -->
         </section>
         <!-- /.content -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Data Testing</h3>
+                            </div>
+                            <div class="card-body">
+                                <table id="myTableTesting" class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>RT/RW</th>
+                                            <th>NIK</th>
+                                            <th>Nama Lengkap</th>
+                                            @foreach ($kriteria as $item)
+                                                <th>{{ $item->nama }}</th>
+                                            @endforeach
+                                            <th>Keputusan</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="text-center">
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Encludean Distance</h3>
+                            </div>
+                            <div class="card-body">
+                                <table id="myTableEncludean" class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>RT/RW</th>
+                                            <th>NIK</th>
+                                            <th>Nama Lengkap</th>
+                                            @foreach ($kriteria as $item)
+                                                <th>{{ $item->nama }}</th>
+                                            @endforeach
+                                            <th>Keputusan</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="text-center">
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Klasifikasi Nearest Neighbors</h3>
+                            </div>
+                            <div class="card-body">
+                                <table id="myTableKlasifikasi" class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>RT/RW</th>
+                                            <th>NIK</th>
+                                            <th>Nama Lengkap</th>
+                                            @foreach ($kriteria as $item)
+                                                <th>{{ $item->nama }}</th>
+                                            @endforeach
+                                            <th>Keputusan</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="text-center">
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Kesimpulan</h3>
+                            </div>
+                            <div class="card-body">
+                                <h1>Hasil Analisa : </h1>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
     </div>
+
+    @if (session('success'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil',
+                text: '{{ session('success') }}'
+            });
+        </script>
+    @endif
+    @if ($errors->any())
+        <script>
+            Swal.fire({
+                icon: 'error',
+                title: 'Oopss...',
+                text: '{{ $errors->first() }}'
+            });
+        </script>
+    @endif
 
     <script>
         $(document).ready(function() {
-            $("#myTable").DataTable({});
+            $("#myTableTraining").DataTable({
+                processing: true,
+                ordering: true,
+                responsive: true,
+                serverSide: true,
+                ajax: "{{ route('analisa') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex'
+                    },
+                    {
+                        data: 'rt_rw',
+                        name: 'rt_rw'
+                    },
+                    {
+                        data: 'nik',
+                        name: 'nik'
+                    },
+                    {
+                        data: 'nama',
+                        name: 'nama'
+                    },
+                    @foreach ($kriteria as $kriteriaItem)
+                        {
+                            data: 'subkriteria.{{ $kriteriaItem->nama }}',
+                            name: '{{ $kriteriaItem->nama }}',
+                            render: function(data) {
+                                return data ? data : 'N/A';
+                            }
+                        },
+                    @endforeach {
+                        data: 'keputusan',
+                        name: 'keputusan',
+                        render: function(data) {
+                            return data ? data : '?';
+                        }
+                    }
+                ],
+                rowCallback: function(row, data, index) {
+                    var dt = this.api();
+                    $(row).attr('data-id', data.id);
+                    $('td:eq(0)', row).html(dt.page.info().start + index + 1);
+                }
+            });
+            $("#myTableTesting").DataTable({});
+            $("#myTableEncludean").DataTable({});
+            $("#myTableKlasifikasi").DataTable({});
+            $('.datatable-input').on('input', function() {
+                var searchText = $(this).val().toLowerCase();
+
+                $('.table tr').each(function() {
+                    var rowData = $(this).text().toLowerCase();
+                    if (rowData.indexOf(searchText) === -1) {
+                        $(this).hide();
+                    } else {
+                        $(this).show();
+                    }
+                });
+            });
         });
     </script>
 @endsection
